19e34c0290799bc213b19ab5bca04a3dbd9bc0e5
Maria Kovatchka <maria.kovatchka@kellogg.northwestern.edu>
2024-03-05 17:03:25 +0000

    First

--- tacostagram.js
+++ tacostagram.js
@@ -1,6 +1,6 @@
 function getPostsFromAPI() {
   // Replace this with the URL of the JSON API that returns an array of image URLs
-  const url = 'YOUR_URL_GOES_HERE';
+  const url = 'https://3000-miakovatchk-tacogramfin-nliquqvb0sk.ws-us108.gitpod.io/posts.json';
 
   // Make a GET request to the API
   fetch(url)
@@ -11,7 +11,7 @@ function getPostsFromAPI() {
         console.log(post);
 
         // html for each post goes here
-        let html = ``;
+        let html = ``
 
         const postsDiv = document.querySelector('#posts');
         postsDiv.innerHTML += html
